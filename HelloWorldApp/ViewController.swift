f72e7eb21b8cc3b148b8a8d981040783fcfa48ac
yoBulat <bulat2659@gmail.com>
2020-10-14 16:20:16 +0300

    Set up the MainStoryBoard and wrote the App logic

--- HelloWorldApp/ViewController.swift
+++ HelloWorldApp/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var toggleTextButton: UIButton!
+    @IBOutlet var helloWorldLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        helloWorldLabel.isHidden = true
+        toggleTextButton.layer.cornerRadius = 15
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func toggleTextButtonPressed() {
+        if helloWorldLabel.isHidden {
+            helloWorldLabel.isHidden.toggle()
+            toggleTextButton.setTitle("Hide Text", for: .normal)
+        } else {
+            helloWorldLabel.isHidden.toggle()
+            toggleTextButton.setTitle("Show Text", for: .normal)
+        }
+    }
+    
 }
 
